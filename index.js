534848956526706037b28684a6a36aae50810768
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-12-07 21:16:29 +0500

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cookies = require("cookie-parser");
 const path = require("path");
+const cors = require("cors");
 // routes
 const homeRouter = require("./routes/homeRouter");
 const rewardsRouter = require("./routes/rewardsRouter");
@@ -11,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookies("cookiesSecretxljnjanmsnxzjij298", {}));
+app.use(cors());
 
 // routes
 app.use("/", homeRouter);
